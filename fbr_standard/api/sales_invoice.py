0a45b1b3848030178c7c9e92f68022d939f07c69
SafdarAliGit <safdar211@gmail.com>
2025-07-30 00:23:17 +0500

    updated

--- fbr_standard/api/sales_invoice.py
+++ fbr_standard/api/sales_invoice.py
@@ -1,8 +1,9 @@
 import frappe
+from frappe.utils import flt
 
 def custom_on_update(doc, method):
     account = frappe.get_doc("Account", "GST - CTPL")
-    rate = float(account.tax_rate) or frappe.throw("Tax rate undefined on GST - CTPL")
+    rate = flt(account.tax_rate) or frappe.throw("Tax rate undefined on GST - CTPL")
 
     for item in doc.items:
         item.custom_tax_rate = rate
